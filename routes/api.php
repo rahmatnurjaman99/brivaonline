71208f109f7351ab08521c3ca1f994d7ff7c087e
Rahmat Nurjaman <rahmat.nurjaman@kly.id>
2026-03-04 10:12:50 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use RahmatNurjaman99\BrivaOnline\Http\Controllers\BrivaController;
 
-Route::post('/snap/v1.0/access-token/b2b', [BrivaController::class, 'accessToken']);
-Route::post('/v1.0/transfer-va/inquiry', [BrivaController::class, 'inquiry']);
-Route::post('/v1.0/transfer-va/payment', [BrivaController::class, 'payment']);
+Route::prefix('snap/v1.0')->group(function () {
+    Route::post('/access-token/b2b', [BrivaController::class, 'accessToken'])->name('access-token.b2b');
+    Route::post('/transfer-va/inquiry', [BrivaController::class, 'inquiry'])->name('transfer-va.inquiry');
+    Route::post('/transfer-va/payment', [BrivaController::class, 'payment'])->name('transfer-va.payment');
+})->as('snap.v1.0.');
